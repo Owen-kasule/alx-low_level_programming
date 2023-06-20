1454aca9d0114344c97b323cfc348300953b98cb
Owen-kasule <owenatug@outlook.com>
2023-06-20 18:19:43 +0300

    putcha14

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,5 +10,5 @@ int main(void)
   _putchar('r');
   _putchar('\n');
 
-  return 0;
+  return (0);
 }
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include "putchar.h"
 
-int _putchar(char c) {
+int _putchar(char c)
+{
   return write(1, &c, 1);
 }
