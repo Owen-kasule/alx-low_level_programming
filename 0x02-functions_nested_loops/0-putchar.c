22f03618416e108b025c789317a3541ba4c7d558
Owen-kasule <owenatug@outlook.com>
2023-06-20 19:35:25 +0300

    abc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,28 @@
 /*
-* File: 0-putchar.c
-* Auth: Muhereza Owen Kasule 
-*
-*/
+ * File: 0-putchar.c
+ * Auth: Muhereza Owen Kasule 
+ *
+ */
 
 #include "main.h"
 
 /**
-* main - Entry point of the program
-*
-* Return: 0 on success
-*/
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
 
 int main(void)
 {
-   _putchar('_');
-   _putchar('p');
-   _putchar('u');
-   _putchar('t');
-   _putchar('c');
-   _putchar('h');
-   _putchar('a');
-   _putchar('r');
-   _putchar('\n');
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
 
   return (0);
 }
