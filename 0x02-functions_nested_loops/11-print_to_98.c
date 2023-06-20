53bc52d17cfd05956350d6e829e95c54bdfefda4
Owen-kasule <owenatug@outlook.com>
2023-06-21 00:22:19 +0300

    abc1123

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,10 +3,10 @@
  * Auth: Muhereza Owen Kasule
  */
 
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98,
+ * print_to_98 - Prints all natural numbers from input to 98,
  *               in order separated by a comma followed by a space.
  * @n: The number to begin counting at.
  */
@@ -15,6 +15,7 @@ void print_to_98(int n)
 	while (n != 98)
 	{
 		printf("%d, ", n);
+
 		if (n > 98)
 			n--;
 		else
