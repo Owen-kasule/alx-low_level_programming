eb43b6293a4a595b3731f3283010ada2242ebc1c
Owen-kasule <owenatug@outlook.com>
2023-06-24 01:48:26 +0300

    debugge

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-main.c
+ * Auth: Carrie Carr
+ */
+
+#include "main.h"
+
+/**
+ * main - Tests the positive_or_negative function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-debugging directory.
+ */
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
